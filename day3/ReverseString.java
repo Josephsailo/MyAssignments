6f297c1932e28e51fc807c5d22774266a0be0eb0
Josephsailo <josephdocuments1@gmail.com>
2024-08-20 20:29:35 +0530

    Add files via upload
    Day3

---
+++ day3/ReverseString.java
@@ -0,0 +1,17 @@
+package week1.day3;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		String value = "Joseph";
+		
+		 char[] charArray = value.toCharArray();
+		
+		for (int i =charArray.length-1 ; i >=0; i--) {
+			System.out.print(charArray[i]);
+		}
+
+	}
+
+}
